18dc44373809ef27bc11af3953d23d2b80c015f1
afeiship <1290657123@qq.com>
2021-05-27 20:02:48 +0800

    feat: update meta fileds infomation

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,12 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/preset-scss'
-  ]
+  ],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
+      ['@babel/plugin-proposal-class-properties', { loose: false }]
+    ]
+  })
 };
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import FormBuilder from 'antd-form-builder';
 import { Form, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 export type Props = { className: string; value: object; onChange: Function };
 
@@ -39,8 +40,23 @@ export default class AntdLogin extends Component<Props> {
   render() {
     const meta = {
       fields: [
-        { key: 'username', label: 'User Name', widgetProps: { autoComplete: 'off' } },
-        { key: 'password', label: 'Password', widget: 'password' }
+        {
+          key: 'username',
+          label: '用户名',
+          rules: [{ required: true, message: '用户名不能为空' }],
+          widgetProps: {
+            autoComplete: 'off',
+            placeholder: '请输入用户名',
+            prefix: <UserOutlined />
+          }
+        },
+        {
+          key: 'password',
+          label: '密码',
+          widget: 'password',
+          rules: [{ required: true, message: '密码不能为空' }],
+          widgetProps: { placeholder: '请输入密码', prefix: <LockOutlined /> }
+        }
       ]
     };
 
@@ -52,7 +68,7 @@ export default class AntdLogin extends Component<Props> {
           <FormBuilder meta={meta} />
           <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
             <Button type="primary" htmlType="submit">
-              Log in
+              登录
             </Button>
           </Form.Item>
         </Form>
